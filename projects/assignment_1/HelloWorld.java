589505fdcaee23109fe40ba0b7749b44b902f8b7
charujamnis <charushila.jamnis@gmail.com>
2021-06-03 20:17:40 -0400

    HelloWorld.java added

--- projects/assignment_1/HelloWorld.java
+++ projects/assignment_1/HelloWorld.java
@@ -1,7 +1,8 @@
 package assignment_1;
 
 public class HelloWorld {
-    final private static String statement = "Marquis Chafdsfsf";
+    //final private static String statement = "Marquis Chafdsfsf";
+    final private static String statement = "Charu Jamnis";
     public static void main (String[] args)  {
         System.out.println(statement);
     }
